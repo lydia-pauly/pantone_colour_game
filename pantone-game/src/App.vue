7689923ef1e5bf6d4963cd9fbe1aa77a03b4a357
Lydia Pauly <pauly.lydia@gmail.com>
2023-12-13 23:58:36 +0000

    Broke basic template trying to compute name and color

---
+++ pantone-game/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="colour-block">
+    <p>{{ color.colorName }}</p>
+  </div>
+
+</template>
+
+<script>
+  import colors from "./assets/pantone-colors.json";
+  console.log(colors);
+
+  export default {
+    data() {
+      return {
+        color : {
+          index : Math.floor(Math.random() * colors['names'].length),
+          colorName : colors['names'][this.index],
+          valueHex : colors['values'][this.index]
+        }
+      }
+    },
+    }
+</script>
+
+<style>
+  .colour-block {
+    width: 100px;
+    height: 100px;
+    background-color: v-bind(valueHex);
+  }
+</style>
