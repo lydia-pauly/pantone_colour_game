6a1ff6c76bc5ab440f6a3d88a06fb3e6da2959b2
Lydia Pauly <pauly.lydia@gmail.com>
2024-01-25 22:11:53 +0000

    Basic game functionality now works, with a timeout

--- pantone-game/src/App.vue
+++ pantone-game/src/App.vue
@@ -1,24 +1,14 @@
 <template>
-  <div class="game-blocks">
-    <div class="colour-block" id="colour-block-1" :style="{ backgroundColor : values[0] }">
-      <p class="colour-name">{{ names[0] }}</p>
-      <p class="colour-value">{{ values[0] }}</p>
+  <div class="game-body">
+    <div class="game-blocks">
+      <div class="colour-block" id="colour-block-0" :style="{ backgroundColor : values[0] }" @click ="checkAnswer($event)"></div>
+      <div class="colour-block" id="colour-block-1" :style="{ backgroundColor : values[1] }" @click ="checkAnswer($event)"></div>
+      <div class="colour-block" id="colour-block-2" :style="{ backgroundColor : values[2] }" @click ="checkAnswer($event)"></div>
+      <div class="colour-block" id="colour-block-3" :style="{ backgroundColor : values[3] }" @click ="checkAnswer($event)"></div>
     </div>
-    <div class="colour-block" id="colour-block-2" :style="{ backgroundColor : values[1] }">
-      <p class="colour-name">{{ names[1] }}</p>
-      <p class="colour-value">{{ values[1] }}</p>
-    </div>
-    <div class="colour-block" id="colour-block-3" :style="{ backgroundColor : values[2] }">
-      <p class="colour-name">{{ names[2] }}</p>
-      <p class="colour-value">{{ values[2] }}</p>
-    </div>
-    <div class="colour-block" id="colour-block-4" :style="{ backgroundColor : values[3] }">
-      <p class="colour-name">{{ names[3] }}</p>
-      <p class="colour-value">{{ values[3] }}</p>
-    </div>
+    <p id="guessing-name"> {{ guessingName }}</p>
+    <p id="verdict">{{ verdict }}</p>
   </div>
-
-
 </template>
 
 <script>
@@ -28,7 +18,10 @@
     data() {
       return {
         names : [],
-        values : []
+        values : [],
+        choice : 0,
+        guessingName : "",
+        verdict : ""
         }
     },
     methods: {
@@ -39,6 +32,23 @@
           this.names.push(colors['names'][index]);
           this.values.push(colors['values'][index]);
         }
+        this.choice = Math.floor(Math.random() * 4);
+        this.guessingName = this.names[this.choice].replace("-", " ");
+        this.guessingName = this.guessingName.charAt(0).toUpperCase() + this.guessingName.slice(1);
+        return
+      },
+
+      checkAnswer(e) {
+        let id = e["srcElement"]["id"];
+        if ((this.choice).toString() === id.charAt(id.length - 1)) {
+          this.verdict = "Correct!";
+        } else {
+          let actualChoice = this.names[(id.charAt(id.length -1))];
+          actualChoice = actualChoice.replace("-", " ");
+          actualChoice = actualChoice.charAt(0).toUpperCase() + actualChoice.slice(1);
+          this.verdict = "Wrong! That was " + actualChoice;
+        }
+        setTimeout("location.reload()", 1500);
         return
       }
     },
@@ -50,6 +60,9 @@
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Cute+Font&family=Oswald:wght@500&family=Roboto+Condensed:wght@700&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Cute+Font&family=Zilla+Slab:wght@500&display=swap');
+
   .colour-block {
     width: 200px;
     height: 200px;
@@ -59,5 +72,21 @@
 
   .game-blocks {
     display: flex;
+    width: 100%;
+  }
+
+  #guessing-name {
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    font-family: 'Zilla Slab', serif;
+    font-size: 80px;
+    color: white;
+
+  }
+
+  .game-body {
+    display: flex;
+    flex-direction: column;
   }
 </style>
