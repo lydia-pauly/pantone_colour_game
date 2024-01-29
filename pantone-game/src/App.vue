c68827c434f527bd53f81c03116ccf83ee1aa1b9
Lydia Pauly <pauly.lydia@gmail.com>
2024-01-29 01:50:21 +0000

    Game now successfully tracks state + reduced flashbangs

--- pantone-game/src/App.vue
+++ pantone-game/src/App.vue
@@ -3,6 +3,9 @@
     <p>Total correct: {{ totalCorrect }}</p>
     <p>Highest streak: {{ largestStreak }}</p>
     <p>Current streak: {{ currentStreak }}</p>
+    <div class="progress-bar">
+        <div class="progress-bar__value" :style="{width : progressWidth+'px'}"></div>
+    </div>
     <div class="game-blocks">
       <div class="colour-and-name">
         <div class="colour-block" :class="{'correct-guess': correctnessArray[0]}" id="colour-block-0" :style="{ backgroundColor : values[0] }" @click ="checkAnswer($event)"></div>
@@ -43,7 +46,8 @@ import colors from "./assets/pantone-colors.json";
         totalCorrect: 0,
         largestStreak: 0,
         currentStreak: 0,
-        streakUnbroken : true
+        streakUnbroken : true,
+        progressWidth : 0,
         }
     },
     methods: {
@@ -65,13 +69,24 @@ import colors from "./assets/pantone-colors.json";
         if ((this.choice).toString() === id.charAt(id.length - 1)) {
           this.guessingName = "Correct!";
           this.totalCorrect++;
+          localStorage.setItem("totalCorrect", this.totalCorrect);
           this.currentStreak++;
+          localStorage.setItem("currentStreak", this.currentStreak);
+          this.progressWidth = (this.currentStreak / 30) * 500;
+          localStorage.setItem("progressWidth", this.progressWidth);
+          if (this.currentStreak > this.largestStreak) {
+            this.largestStreak = this.currentStreak;
+            localStorage.setItem("largestStreak", this.currentStreak);
+          }
         } else {
           // let actualChoice = this.names[(id.charAt(id.length -1))];
           // actualChoice = actualChoice.replace("-", " ");
           // actualChoice = actualChoice.charAt(0).toUpperCase() + actualChoice.slice(1);
           this.guessingName = "Incorrect!";
           this.currentStreak = 0;
+          localStorage.setItem("currentStreak", 0);
+          this.progressWidth = 0;
+          localStorage.setItem("progressWidth", 0);
         }
         for (let i = 0; i < 4; i++) {
           // if (i === this.choice) {
@@ -83,11 +98,41 @@ import colors from "./assets/pantone-colors.json";
         }
         setTimeout("location.reload()", 2000);
         return
+      },
+
+      setLocalStorage() {
+        if (localStorage.getItem("totalCorrect")) {
+          this.totalCorrect = localStorage.getItem("totalCorrect");
+        } else {
+          this.totalCorrect = 0;
+          localStorage.setItem("totalCorrect", 0);
+        }
+        if (localStorage.getItem("largestStreak")) {
+          this.largestStreak = localStorage.getItem("largestStreak");
+        } else {
+          this.largestStreak = 0;
+          localStorage.setItem("largestStreak", 0);
+        }
+
+        if (localStorage.getItem("currentStreak")) {
+          this.currentStreak = localStorage.getItem("currentStreak");
+        } else {
+          this.currentStreak = 0;
+          localStorage.setItem("currentStreak", 0);
+        }
+
+        if (localStorage.getItem("progressWidth")) {
+          this.progressWidth = localStorage.getItem("progressWidth");
+        } else {
+          this.progressWidth = 0;
+          localStorage.setItem("progressWidth", 0);
+        }
       }
     },
 
     mounted() {
       this.setNamesAndValues();
+      this.setLocalStorage();
     }
     }
 </script>
@@ -132,11 +177,25 @@ import colors from "./assets/pantone-colors.json";
   .game-body {
     display: flex;
     flex-direction: column;
+    justify-content: center;
   }
 
   .correct-guess {
     border-color: #6ed475;
     border-width: 10px;
     border-style: solid;
+  }
+
+  .progress-bar {
+    width: 500px;
+    height: 3em;
+    border-radius: 10em;
+    background-color: black;
+  }
+
+  .progress-bar__value {
+    height: 3em;
+    border-radius: 10em;
+    background-color: red;
   }
 </style>
