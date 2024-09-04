dc576055c4d6cacd3ce829852901bf0b939a0441
Lydia Pauly <pauly.lydia@gmail.com>
2024-09-04 23:51:50 +0100

    Trying to add reload feature but getting stuck in endless reloading loops

--- pantone_game_svelte/src/lib/ColourNameGuess.svelte
+++ pantone_game_svelte/src/lib/ColourNameGuess.svelte
@@ -1,5 +1,12 @@
 <script>
-  export let chosen_colour = "not yet set";
+  export let chosen_colour = "";
 </script>
 
-<p>{chosen_colour}</p>
+<p class="colour main-text">{chosen_colour.replaceAll("-", " ")}</p>
+
+<style>
+  p {
+    margin: 2em auto;
+    font-size: 2em;
+  }
+</style>
--- pantone_game_svelte/src/routes/+page.svelte
+++ pantone_game_svelte/src/routes/+page.svelte
@@ -1,10 +1,26 @@
 <script>
+  //Import components from /lib
   import ColourSquare from "$lib/ColourSquare.svelte";
-  import colourJSON from "$lib/pantone-colors.json";
   import ColourNameGuess from "$lib/ColourNameGuess.svelte";
+
+  //Import methods
   import { onMount } from "svelte";
 
+  //Import data
+  import colourJSON from "$lib/pantone-colors.json";
+
+  //Initialise variables
+  let hex_value_array = [];
+  let colour_name_array = [];
+  let chosen_colour = "";
+  let hidden = false;
+
+  //Set up initial game state upon mount by selecting colours to guess and their respective names
   onMount(() => {
+    setGame(true);
+  });
+
+  function setGame(no_reload) {
     hex_value_array = [];
     colour_name_array = [];
     for (let step = 0; step < 4; step++) {
@@ -13,11 +29,14 @@
       colour_name_array = [...colour_name_array, colourJSON["names"][selector]];
     }
     chosen_colour = colour_name_array[Math.floor(Math.random() * 4)];
-  });
+    if (!no_reload) {
+      //location.reload();
+    }
+  }
 
-  let hex_value_array = [];
-  let colour_name_array = [];
-  let chosen_colour = "not yet defined";
+  function changeHiddenProperty() {
+    hidden = !hidden;
+  }
 </script>
 
 <div class="colour-square-bar">
@@ -25,19 +44,28 @@
     <ColourSquare
       --square-colour={hex_value_array[i]}
       colour_name={colour_name_array[i]}
+      {hidden}
     />
   {/each}
 </div>
 
-<ColourNameGuess {chosen_colour} />
+<div class="colour-guess main-text">
+  <ColourNameGuess {chosen_colour} />
+</div>
 
-<!-- <button on:click={setColours}>Set colours</button> -->
+<button on:click={changeHiddenProperty}>Change hidden</button>
+<button on:click={setGame()}>Reload game</button>
 
 <style>
   .colour-square-bar {
     display: flex;
     align-items: center;
     flex-wrap: wrap;
+    margin: auto;
+  }
+
+  .colour-guess {
+    display: flex;
     margin: auto;
   }
 </style>
